1674865d1507d447e68428f3a0d45faf3bb6488d
gminnimk <tvt1245@naver.com>
2024-05-30 10:45:57 +0900

    init commit

--- src/main/java/com/sparta/springauth/Config/PasswordConfig.java
+++ src/main/java/com/sparta/springauth/Config/PasswordConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+
+// 아래와 같이 설정하면 Spring 서버가 뜰 떄
+// Spring IoC Container에 이게 Bean으로 저장될 것.
 @Configuration
 public class PasswordConfig {
 
